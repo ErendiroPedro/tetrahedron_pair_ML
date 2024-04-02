304f1c680153d77ffb4d5a3e02f7f6f01530d660
erendiro pedro <erendiropedro@gmail.com>
2024-04-02 16:40:35 +0200

    increase model complexity

--- src/model_architecture.py
+++ src/model_architecture.py
@@ -4,10 +4,11 @@ import torch.nn.functional as F
 
 class GraphPairClassifier(torch.nn.Module):
     def __init__(self, num_input_features):
+        torch.manual_seed(12345)
         super(GraphPairClassifier, self).__init__()
         
-        self.encoder = GCNEncoder(num_input_features, 64)
-        self.classifier = MLPClassifier(128, 1)
+        self.encoder = GCNEncoder(num_input_features, 256)
+        self.classifier = MLPClassifier(512, 1)
 
     def forward(self, data):
 
@@ -27,20 +28,25 @@ class GCNEncoder(torch.nn.Module):
     def __init__(self, num_input_features, num_output_features):
         super(GCNEncoder, self).__init__()
         self.conv1 = GCNConv(num_input_features, num_output_features)
+        self.conv2 = GCNConv(num_output_features, num_output_features)
+        self.conv3 = GCNConv(num_output_features, num_output_features)
 
     def forward(self, x, edge_index, batch):
         x = F.relu(self.conv1(x, edge_index))
+        x = F.relu(self.conv2(x, edge_index))
+        x = F.relu(self.conv3(x, edge_index))
         x = global_mean_pool(x, batch)  # Get whole graph embeddings
-        print(x.shape)
         return x
 
 class MLPClassifier(torch.nn.Module):
     def __init__(self, num_input_features, num_output_features):
         super(MLPClassifier, self).__init__()
-        self.layer_1 = torch.nn.Linear(num_input_features, 64)
-        self.layer_2 = torch.nn.Linear(64, 32)
-        self.layer_3 = torch.nn.Linear(32, 16)
-        self.layer_4 = torch.nn.Linear(16, num_output_features)
+        self.layer_1 = torch.nn.Linear(num_input_features, 512)
+        self.layer_2 = torch.nn.Linear(512, 256)
+        self.layer_3 = torch.nn.Linear(256, 128)
+        self.layer_4 = torch.nn.Linear(128, 64)
+        self.layer_5 = torch.nn.Linear(64, 32)
+        self.layer_6 = torch.nn.Linear(32, num_output_features)
         
     def forward(self, x):
         x = F.relu(self.layer_1(x))
