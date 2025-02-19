1d4cb96cc70cf08b43e56b57b06f74a9147bee86
Erendiro Pedro <erendiropedro@gmail.com>
2025-02-19 09:58:53 +0000

    infer shape from data

--- src/ModelBuilder.py
+++ src/ModelBuilder.py
@@ -1,5 +1,7 @@
 import torch.nn.functional as F
 from src.Architectures import MLP, DeepSet, PointNet
+import pandas as pd
+import os
 
 class ModelBuilder:
     def __init__(self, config):
@@ -8,6 +10,22 @@ class ModelBuilder:
         """
         self.config = config
         self.task = config['task']
+
+    def _infer_input_shape(self):
+        """
+        Load dataset from the path and infer input shape.
+        """
+        data_path = os.path.join(self.config.get('processed_data_path'), "train", "train_data.csv")
+        if not data_path:
+            raise ValueError("Processed data path is not specified in the config.")
+
+        try:
+            sample_data = pd.read_csv(data_path, nrows=2)
+        except Exception as e:
+            raise ValueError(f"Error loading dataset: {e}")
+
+        example_input = sample_data.iloc[:, :-2]  # Exclude labels
+        return example_input.shape[1]  # Number of input features
 
     def build(self):
         """
@@ -18,18 +36,21 @@ class ModelBuilder:
         """
         
         print("-- Building Architecture --")
+
         model = None
+        input_shape = self._infer_input_shape()
+
         if self.config['architecture'] == 'mlp':
             model = MLP(
-                input_dim=24,  # 4 points × 3 coordinates × 2 tetrahedra
-                hidden_dims=[128],
+                input_dim=input_shape,
+                hidden_dims=[128, 128, 128, 128],
                 activation=self.config['activation_function'],
                 dropout_rate=self.config['dropout_rate'],
                 task=self.task
             )
         elif self.config['architecture'] == 'deep_set':
             model = DeepSet(
-                point_dim=3,  # Each point has x, y, z coordinates
+                point_dim=3,
                 hidden_dims=[128, 128, 128],
                 activation=self.config['activation_function'],
                 dropout_rate=self.config['dropout_rate'],
@@ -37,8 +58,8 @@ class ModelBuilder:
             )
         elif self.config['architecture'] == 'pointnet':
             model = PointNet(
-                point_dim=3,  # Each point has x, y, z coordinates
-                hidden_dims=[64, 128, 256],  # Minimal hidden dimensions
+                point_dim=3,
+                hidden_dims=[64, 128, 256],
                 activation=self.config['activation_function'],
                 dropout_rate=self.config['dropout_rate'],
                 task=self.task
